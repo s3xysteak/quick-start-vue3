0f29351dc42174d9f601bf7d17c0af69e33bb1f9
s3xysteak <793744342@qq.com>
2023-11-13 14:36:26 +0800

    build: 添加依赖

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import router from '@/router'
 import '@/styles/main.css'
 import 'virtual:uno.css'
 
+import 'ant-design-vue/dist/reset.css';
+
 const app = createApp(App)
 
 app.use(pinia).use(router)
+
+Object.defineProperty(globalThis, 'CESIUM_BASE_URL', {
+  value: '/libs/cesium'
+})
 
 router.isReady().then(() => {
   app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
@@ -15,7 +21,24 @@ export default defineConfig({
     UnoCSS(),
     AutoImport({
       imports: ['vue']
-    })
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false
+        })
+      ]
+    }),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          cesiumInjectScript: `<script src="libs/cesium/Cesium.js"></script>`
+        }
+      }
+    }),
+    copyCesium(['Assets', 'ThirdParty', 'Widgets', 'Workers']),
+    viteExternalsPlugin({ cesium: 'Cesium' }, { disableInServe: true })
   ],
   resolve: {
     alias: {
@@ -23,3 +46,22 @@ export default defineConfig({
     }
   }
 })
+
+/**
+ * @param {string[]} items - 文件夹名数组
+ * @returns {Plugin[]}
+ */
+function copyCesium(items) {
+  return viteStaticCopy({
+    targets: [
+      ...items.map(item => ({
+        src: `node_modules/cesium/Build/Cesium/${item}/*`,
+        dest: `libs/cesium/${item}/`
+      })),
+      {
+        src: 'node_modules/cesium/Build/Cesium/Cesium.js',
+        dest: 'libs/cesium/'
+      }
+    ]
+  })
+}
